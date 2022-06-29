d411de1451875bcac7f7a2e562bcf4bb9c4a3b0b
tuantea <tuan1722000@gmail.com>
2022-06-29 17:05:50 +0700

    Login by Authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 /* スタイルシート */
 import './styles/main.css';
 
 /* コンポーネント */
 import Todo from './components/Todo';
+import Login from "./components/Login";
+
+import { auth, storeUserInfo } from "./lib/firebase";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      setLoading(false);
+      let newUser = null;
+      if (user) {
+        newUser = await storeUserInfo(user);
+      }
+      setUser(newUser);
+    });
+  }, []);
+
+  const logout = () => {
+    auth.signOut();
+  };
+
+
+  const Header = () => {
+    if (user) {
+      return (
+        <div class="navbar-end">
+          <div class="navbar-item">
+            {user.name}
+          </div>
+          <div class="navbar-item">
+            <button class="button is-danger  is-small" onClick={logout} > Logout</button>
+          </div>
+        </div >
+      )
+    } else {
+      return (<Login />)
+    }
+  }
   return (
     <div className="container is-fluid">
-      <Todo />
-    </div>
+       <header class="navbar">
+        {loading ? (
+          <p>
+            LOADING.....
+          </p>
+        ) : (
+          <Header/>
+        )}
+      </header >
+      <div>
+        {user && <Todo />}
+      </div>
+    </div >
   );
 }
 
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -60,3 +60,50 @@ export const clearFirebaseItem = async (item) => {
     console.log(err);
   });
 };
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await database.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await database.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}
+
+export const updateUser = async (user, image) => {
+  try {
+    const userDoc = await firebase.firestore().collection("users").doc(user.id).get();
+    if (userDoc.exists) {
+      await firebase.firestore().collection("users").doc(user.id).update({ ...userDoc.data(), image: image });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const uploadImage = async (image) => {
+  const ref = firebase.storage().ref().child(`/images/${image.name}`);
+  let downloadUrl = "";
+  try {
+    await ref.put(image);
+    downloadUrl = await ref.getDownloadURL();
+  } catch (err) {
+    console.log(err);
+  }
+  return downloadUrl;
+}; 
